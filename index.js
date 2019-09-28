bc81d5a274c3b179cb231ebbc8d450aaf12a4008
Christophe Parmentier <christophe.parmentier@ineat-conseil.fr>
2019-09-28 17:19:47 +0200

    feat(keycloak): Implementation of Keycloak

--- index.js
+++ index.js
@@ -1,18 +1,41 @@
 import express from 'express';
 import bodyParser from 'body-parser';
+import session from 'express-session';
+import Keycloak from 'keycloak-connect';
 
 const app = express();
 app.use(bodyParser.json());
+const memoryStore = new session.MemoryStore();
+
+app.use(
+  session({
+    secret: 'secretKey',
+    resave: false,
+    saveUninitialized: true,
+    store: memoryStore
+  })
+);
+
+const keycloak = new Keycloak({
+  store: memoryStore
+});
+
+app.use(
+  keycloak.middleware({
+    logout: '/logout',
+    admin: '/'
+  })
+);
 
 app.get('/api/unsecured', function(req, res) {
   res.json({ message: 'This is an unsecured endpoint payload' });
 });
 
-app.get('/api/user', function(req, res) {
+app.get('/api/user', keycloak.protect('realm:user'), function(req, res) {
   res.json({ message: 'This is an USER endpoint payload' });
 });
 
-app.get('/api/admin', function(req, res) {
+app.get('/api/admin', keycloak.protect('realm:admin'), function(req, res) {
   res.json({ message: 'This is an ADMIN endpoint payload' });
 });
 
